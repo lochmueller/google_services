3bbab1422ae8307fd4fcaa658819658f1d6e768b
Tim Lochmüller <tim@fruit-lab.de>
2016-06-14 19:48:54 +0200

    [TASK] Add travis support

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array(
     'author_email' => 'webmaster@fruit-lab.de',
     'constraints'  => array(
         'depends' => array(
-            'typo3' => '6.2.0-7.99.99',
+            'typo3' => '6.2.0-8.1.99',
         ),
     ),
 );
